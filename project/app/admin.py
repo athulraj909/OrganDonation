c87d4a2859af56f92640b3a7cef3b773c02fe5b5
athulraj909 <athulrajn1999@gmail.com>
2024-01-17 16:22:56 +0530

    changed

--- project/app/admin.py
+++ project/app/admin.py
@@ -4,22 +4,74 @@ from .models import hospital,receiver,donor,Request
 
 class HospitalAdmin(admin.ModelAdmin):
     list_display = ('Hospital_name', 'phone_no', 'Email_id', 'location', 'status')
-    fields = ('Hospital_name', 'phone_no', 'Email_id', 'location', 'status')
+    readonly_fields = ('Hospital_name', 'phone_no', 'Email_id', 'location')
+    exclude = ('password',)
+
+    def has_add_permission(self, request):
+        # Allow adding new objects
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        # Allow changing existing objects
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        # Allow deleting existing objects
+        return True
+    
 
 
 class ReceiverAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone_no', 'Email_id', 'Address', 'hospital_name', 'status')
-    fields = ('name', 'phone_no', 'Email_id', 'Address', 'hospital_name', 'status')
+    readonly_fields = ('name', 'phone_no', 'Email_id', 'Address', 'hospital_name')
+    exclude = ('password',)  # Exclude the 'password' field from the change form
+
+    def has_add_permission(self, request):
+        # Allow adding new objects
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        # Allow changing existing objects
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        # Allow deleting existing objects
+        return True
 
 
 class DonorAdmin(admin.ModelAdmin):
-    list_display = ('name','Organ_name','gender','age','bloodgroup','phone','email')
-    fields = ('hospital_id','name','doctername','Organ_name','gender','age','bloodgroup','phone','email','address','photo','idproof')
+    list_display = ('name', 'Organ_name', 'gender', 'age', 'bloodgroup', 'phone', 'email')
+    readonly_fields = ('name', 'Organ_name', 'gender', 'age', 'bloodgroup', 'phone', 'email','hospital_id', 'doctername', 'address', 'photo', 'idproof')
+
+
+    def has_add_permission(self, request):
+        # Allow adding new objects
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        # Allow changing existing objects
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        # Allow deleting existing objects
+        return True
 
 
 class RequestAdmin(admin.ModelAdmin):
     list_display = ('reciver_id', 'hospital_id', 'donor_id', 'organ_name', 'status')
-    fields = ('reciver_id', 'hospital_id', 'donor_id', 'organ_name', 'certificate', 'status')
+    readonly_fields = ('reciver_id', 'hospital_id', 'donor_id','organ_name', 'certificate','status')
+
+    def has_add_permission(self, request):
+        # Allow adding new objects
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        # Allow changing existing objects
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        # Allow deleting existing objects
+        return True
 
 
 admin.site.register(hospital,HospitalAdmin)
